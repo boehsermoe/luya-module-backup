cbb043667dbf5f92cae3a0838d9c16c80a97dff3
boehsermoe <boehsermoe@me.com>
2018-06-01 00:23:05 +0200

    Documentation

--- src/Module.php
+++ src/Module.php
@@ -9,6 +9,8 @@ use luya\backup\schedules\FileJob;
  * Backup Admin Module.
  *
  * File has been created with `module/create` command.
+ *
+ * @property $exportDir The path where the backup files will be exported. Default @runtime/backups
  */
 class Module extends \luya\admin\base\Module
 {
